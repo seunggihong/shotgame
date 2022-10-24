c21a213be18c5ed9c3b2d7ffe0a5db6f56a36741
seunggihong <hsk1533@naver.com>
2022-10-24 23:06:13 +0900

    enemy controll

--- Projectile.cs
+++ Projectile.cs
@@ -4,13 +4,30 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
+    public LayerMask collisionMask;
     float speed = 10;
     
     public void SetSpeed(float newSpeed) {
         speed = newSpeed;
     }
-    
+
     void Update () {
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        float moveDistance = speed * Time.deltaTime;
+        CheckCollisions(moveDistance);
+        transform.Translate(Vector3.forward * moveDistance);
+    }
+
+    void CheckCollisions(float moveDistance) {
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hit;
+
+        if(Physics.Raycast(ray, out hit, moveDistance, collisionMask, QueryTriggerInteraction.Collide)){
+            OnHitObject(hit);
+        }
+    }
+
+    void OnHitObject(RaycastHit hit) {
+        print(hit.collider.gameObject.name);
+        GameObject.Destroy(gameObject);
     }
 }
