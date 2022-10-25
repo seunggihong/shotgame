c29a70989c6ae39bc4a654f7810c56689975e21e
seunggihong <hsk1533@naver.com>
2022-10-26 01:23:41 +0900

    enemy attack controll

--- Enemy.cs
+++ Enemy.cs
@@ -6,29 +6,84 @@ using UnityEngine.AI;
 [RequireComponent (typeof (NavMeshAgent))]
 public class Enemy : LivingEntity
 {
+    public enum State {Idle, Chasing, Attacking};
+    State currentState;
+
     NavMeshAgent pathfinder;
     Transform target;
+    Material skinMaterial;
 
-    protected override void Start()
-    {
+    Color originalColor;
+
+    float attackdistanceThreshold = 1.5f;
+    float timeBetweenAttacks = 1;
+
+    float nextAttackTime;
+    float myCollisionRadius;
+    float targetCollisionRadius;
+
+    protected override void Start() {
         base.Start();
         pathfinder = GetComponent<NavMeshAgent>();
+        skinMaterial = GetComponent<Renderer>().material;
+        originalColor = skinMaterial.color;
+
+        currentState = State.Chasing;
         target = GameObject.FindGameObjectWithTag("Player").transform;
+
+        myCollisionRadius = GetComponent<CapsuleCollider>().radius;
+        targetCollisionRadius = target.GetComponent<CapsuleCollider>().radius;
+
         StartCoroutine(UpdatePath());
     }
 
-    void Update()
-    {
+    void Update() {
+        if(Time.time > nextAttackTime) {
+            float sqrDstToTarget = (target.position - transform.position).sqrMagnitude;
+            if(sqrDstToTarget < Mathf.Pow(attackdistanceThreshold, 2)) {
+                nextAttackTime = Time.time + timeBetweenAttacks;
+                StartCoroutine(Attack());
+            } 
+        }
+        
+    }
 
+    IEnumerator Attack() {
+
+        currentState = State.Attacking;
+        pathfinder.enabled = false;
+
+        Vector3 originalPosition = transform.position;
+        Vector3 attackPosition = target.position;
+
+        float attackSpeed = 3;
+        float percent = 0;
+
+        skinMaterial.color = Color.red;
+
+        while(percent <= 1) {
+            percent += Time.deltaTime * attackSpeed;
+            float interpolation = ( -Mathf.Pow(percent,2) + percent ) * 4;
+            transform.position = Vector3.Lerp(originalPosition,attackPosition,interpolation);
+            yield return null;
+        }
+
+        skinMaterial.color = originalColor;
+        currentState = State.Chasing;
+        pathfinder.enabled = true;
     }
 
     IEnumerator UpdatePath() {
         float refreshRate = .25f;
         while (target != null) {
-            Vector3 targetPosition = new Vector3(target.position.x,0,target.position.z);
-            if(!dead) {
-                pathfinder.SetDestination(targetPosition);
+            if(currentState == State.Chasing) {
+                Vector3 dirToTarget = (target.position - transform.position).normalized;
+                Vector3 targetPosition = target.position - dirToTarget * (myCollisionRadius + targetCollisionRadius);
+                if(!dead) {
+                    pathfinder.SetDestination(targetPosition);
+                } 
             }
+            
             yield return new WaitForSeconds(refreshRate);
         }
     }
--- GunController.cs
+++ GunController.cs
@@ -10,7 +10,7 @@ public class GunController : MonoBehaviour
 
     void Start() {
        if(startingGun != null) {
-        EquipGun(startingGun);
+            EquipGun(startingGun);
        }
 
     }
